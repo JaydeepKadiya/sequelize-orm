4df34cefb6aa71d21560885078e978c3e2a523fb
JaydeepKadiya <jaydeep.kadiya@aeonx.digital>
2024-02-08 10:25:59 +0530

    comment s3

--- app/s3.js
+++ app/s3.js
@@ -1,19 +1,19 @@
-const fs = require("fs")
-const AWS = require("aws-sdk")
-const dotenv = require("dotenv")
-dotenv.config()
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-const region = process.env.AWS_REGION
-const bucketName = process.env.AWS_BUCKET_NAME
+// const AWS = require("aws-sdk")
+// const dotenv = require("dotenv")
+// dotenv.config()
+
+// const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+// const region = process.env.AWS_REGION
+// const bucketName = process.env.AWS_BUCKET_NAME
 
 
-const s3 = new AWS.S3({
-    region,
-    accessKeyId,
-    secretAccessKey
-});
+// const s3 = new AWS.S3({
+//     region,
+//     accessKeyId,
+//     secretAccessKey
+// });
 
 // uploads file to s3
 
